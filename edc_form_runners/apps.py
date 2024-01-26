e1c9decc7b23165cdb0cdad6d2cfb944a056cfe6
erikvw <ew2789@gmail.com>
2024-01-26 00:32:37 -0600

    move post-migrate, autodiscover and/or system checks to edc-appconfig

--- edc_form_runners/apps.py
+++ edc_form_runners/apps.py
@@ -2,8 +2,6 @@ import sys
 
 from django.apps import AppConfig as DjangoAppConfig
 
-from .site import site_form_runners
-
 
 class AppConfig(DjangoAppConfig):
     name = "edc_form_runners"
@@ -13,7 +11,3 @@ class AppConfig(DjangoAppConfig):
     include_in_administration_section = True
     has_exportable_data = True
     default_auto_field = "django.db.models.BigAutoField"
-
-    def ready(self):
-        sys.stdout.write(f"Loading {self.verbose_name} ...\n")
-        site_form_runners.autodiscover()
