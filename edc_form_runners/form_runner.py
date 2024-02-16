ba85933d343c28a034ece86262a9112216f77cdc
erikvw <ew2789@gmail.com>
2024-02-15 20:38:13 -0600

    fix unpacking message, default to modelform fields if no fieldsets, add tests and testapp

--- edc_form_runners/form_runner.py
+++ edc_form_runners/form_runner.py
@@ -68,8 +68,8 @@ class FormRunner:
         for src_obj in tqdm(self.src_qs, total=total):
             self.issue_model_cls.objects.filter(**self.unique_opts(src_obj)).delete()
             self.run_one(src_obj=src_obj, skip_delete=True)
-        for model_name, message in self.messages.values():
-            print(f"{model_name}: {message}")
+        for k, v in self.messages.items():
+            print(f"Warning: {k}: {v}")
 
     def run_one(self, src_obj: Model, skip_delete: bool | None = None) -> None:
         if not skip_delete:
@@ -100,9 +100,17 @@ class FormRunner:
                     _, data = fieldset
                     fields.extend(data.get("fields"))
             else:
-                self.messages.update({self.model_name: "Fieldsets not defined. Using fields."})
+                self.messages.update(
+                    {
+                        self.model_name: (
+                            "ModelAdmin fieldsets not defined. Using ModelForm fields."
+                        )
+                    }
+                )
 
-                fields = getattr(self.modeladmin_cls, "fields", [])
+                fields = [
+                    k for k, v in getattr(self.modeladmin_cls.form(), "fields", {}).items()
+                ]
         fields = list(set(fields))
         return fields
 
